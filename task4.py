9f391947d71f4a72f006afe07e99104b3df03d7a
sherm3 <mamafiendaasd@gmail.com>
2022-12-12 01:12:10 +0300

    t4

---
+++ task4.py
@@ -0,0 +1,46 @@
+'''
+Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+aaaasssdddwwwwwwwwwwwweeeffffff -> 4a3s3d9w3w3e6f
+4a3s3d9w3w3e6f-> aaaasssdddwwwwwwwwwwwweeeffffff
+'''
+
+with open('decoded.txt', 'r') as data:
+    my_text = data.read()
+
+def encode_rle(ss):
+    str_code = ''
+    prev_char = ''
+    count = 1
+    for char in ss:
+        if char != prev_char:
+            if prev_char:
+                str_code += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    return str_code
+
+            
+str_code = encode_rle(my_text)
+print(str_code)
+
+with open('encoded.txt', 'r') as data:
+    my_text2 = data.read()
+
+def decoding_rle(ss:str):
+    count = ''
+    str_decode = ''
+    for char in ss:
+        if char.isdigit():
+            count += char 
+        else:
+            str_decode += char * int(count)
+            count = ''
+    return str_decode
+
+str_decode = decoding_rle(my_text2)
+print(str_decode)
